75657feb0de92bfd681328e5653dfaed9d49366c
APBellan <allanbellan92@gmail.com>
2022-06-07 00:42:40 -0300

    Pagina Web 1.0 - buttons

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ def index():
     posts_reserv = conn.execute('SELECT * FROM reserv ORDER BY ID DESC LIMIT 1').fetchall()
     conn.close()
 
+    try:
+        if request.args['teste'] == '1':
+            print("deu boaaaaa 111111111111")
+        elif request.args['teste'] == '2':
+            print("deu boaaaaa 222222222222")
+        elif request.args['teste'] == '3':
+            print("deu boaaaaa 333333333333")
+    except:
+        pass
+
     return render_template('index.html', posts_humid=posts_humid, posts_dht11=posts_dht11, posts_reserv=posts_reserv)
 
 @app.route('/humid')
@@ -30,6 +40,19 @@ def post_humid():
 def post_dht11():
     post_dht11 = get_post_dht11()
     return render_template('dht11.html', post_dht11=post_dht11)
+
+@app.route('/reserv')
+def post_reserv():
+    post_reserv = get_post_reserv()
+    return render_template('reserv.html', post_reserv=post_reserv)
+
+@app.route('/push')
+def teste_push():
+    strprint = "DEU CERTOOOOOOOO _______________"
+    print(strprint)
+    
+
+    return render_template('index.html')
 
 # def teste(str_teste):
 #     str_teste = str_teste
@@ -52,3 +75,11 @@ def get_post_dht11():
     if post_dht11 is None:
         abort(404)
     return post_dht11
+    
+def get_post_reserv():
+    conn = get_db_connection()
+    post_reserv = conn.execute('SELECT * FROM reserv ORDER BY ID DESC LIMIT 17').fetchall()
+    conn.close()
+    if post_reserv is None:
+        abort(404)
+    return post_reserv
